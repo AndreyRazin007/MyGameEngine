1adff30ba155efd8fa17852a0070229f7dfaee20
AndreyRazin <110714747+AndreyRazin007@users.noreply.github.com>
2023-07-21 21:34:40 +0500

    OpenGL and glfw initialization

---
+++ src/constants.hpp
@@ -0,0 +1,2 @@
+int g_window_size_x{ 1280 };
+int g_window_size_y{ 720 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,82 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+/* Подключение постоянных значений для OpenGL */
+#include "constants.hpp"
+
 #include <iostream>
 
-int main(void)
+/* Функция для обработки изменения размера окна */
+void glfw_window_size_callback(GLFWwindow *window, int width, int height)
 {
-    GLFWwindow* window;
-    if (!glfwInit())
-        return -1;
+    g_window_size_x = width;
+    g_window_size_y = height;
 
-    window = glfwCreateWindow(1280, 700, "MyGame", nullptr, nullptr);
-    if (!window)
+    glViewport(0, 0, g_window_size_x, g_window_size_y);
+}
+
+/* Функция для обработки закрытия окна */
+void glfw_key_callback(GLFWwindow* p_window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
-        glfwTerminate();
-        return -1;
+        glfwSetWindowShouldClose(p_window, GL_TRUE);
+    }
+}
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "glfwInit() failed!\n";
+        exit(-1);
     }
 
-    glfwMakeContextCurrent(window);
+    /* Указание используемой версии OpenGL */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    /* Указании спецификации OpenGL */
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *p_window = glfwCreateWindow(g_window_size_x, g_window_size_y,
+        "MyGame", nullptr, nullptr);
+
+    if (!p_window)
+    {
+        std::cerr << "glfwCreateWindow() failed!\n";
+        glfwTerminate();
+        exit(-1);
+    }
+
+    /* Изменение размера окна */
+    glfwSetWindowSizeCallback(p_window, glfw_window_size_callback);
+
+    /* Закрытие окна */
+    glfwSetKeyCallback(p_window, glfw_key_callback);
+
+    glfwMakeContextCurrent(p_window);
 
     if (!gladLoadGL())
     {
-        std::cout << "Can't load GLAD!" << '\n';
-        return -1;
+        std::cerr << "Can't load GLAD!" << '\n';
+        exit(-1);
     }
 
-    std::cout << "OpenGL " << GLVersion.major << '.' << GLVersion.minor << '\n';
-
+    /* Цвет заливки окна */
     glClearColor(1, 0, 0, 1);
 
-    while (!glfwWindowShouldClose(window))
+    /* Главный цикл обработки изображения */
+    while (!glfwWindowShouldClose(p_window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(p_window);
 
         glfwPollEvents();
     }
 
+    /* Очистка ресурсов и уничтожение приложения OpenGL */
     glfwTerminate();
     return 0;
 }
-
